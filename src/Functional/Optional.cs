f37a7a44f8dc4de2f2bf957d72c94bf37fab44b3
Rade Milovic <r.milovic90@gmail.com>
2020-02-02 11:01:38 +0100

    Introduce Optional type with the ability to accept some value.

---
+++ src/Functional/Optional.cs
@@ -0,0 +1,15 @@
+﻿namespace UtilitiesAndAbstractions.Functional
+{
+    public static class Optional
+    {
+        public static Optional<T> OfSome<T>(T value) => new Some<T>();
+    }
+
+    public abstract class Optional<T>
+    {
+    }
+
+    public sealed class Some<T> : Optional<T>
+    {
+    }
+}
---
+++ test/Functional/OptionalTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace UtilitiesAndAbstractions.Functional
+{
+    public sealed class OptionalTests
+    {
+        [Fact]
+        public void AcceptsSomeValue()
+        {
+            var optional = Optional.OfSome("Some value");
+
+            optional.Should().BeOfType(typeof(Some<string>));
+        }
+    }
+}
